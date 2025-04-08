edd37df0dce833a34f9ee54128cd4b3915b299dc
MichelHoffmann <mic.hoff.jr@gmail.com>
2025-04-08 10:17:21 -0500

    style: reorganizando estilos da page Home

--- app/(tabs)/app.tsx
+++ app/(tabs)/app.tsx
@@ -1,40 +1,74 @@
-import { Button, ImageBackground, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
-    return (
-        <View style={style.container}>
-            <ImageBackground style={style.image} source={require('../../assets/images/bg-start.png')} >
-                <Text style={style.tittle}>RunFlow</Text>
-                <Text style={style.text}>Seu progresso começa com o primeiro passo. Corra e evolua!</Text>
-                <Button title="Button"/>
-            </ImageBackground>
-        </View>
-    )
+  return (
+    <View style={style.container}>
+      <ImageBackground
+        style={style.image}
+        source={require("../../assets/images/bg-start.png")}
+      >
+        <Text style={style.title}>RunFlow</Text>
+        <Text style={style.text}>
+          Seu progresso começa com o primeiro passo. Corra e evolua!
+        </Text>
+        <TouchableOpacity style={style.button}>
+            <Text style={style.buttonText}>Começar</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </View>
+  );
 }
 
 const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#232323',
-    },
+  container: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#232323",
+  },
 
-    image: {
-        height: '100%',
-        width: '100%',
-        resizeMode: 'cover',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        paddingBottom: 80,
-        gap: 25
-    },
-    tittle: {
-        fontSize: 30,
-        fontWeight: 'semibold',
-        color: '#fff'
-    },
-    text: {
-        fontSize: 14,
-        fontWeight: 'regular',
-        color: '#fff'
-    }
-})
+  image: {
+    height: "100%",
+    width: "100%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    gap: 5,
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: "semibold",
+    color: "#fff",
+  },
+
+  text: {
+    fontSize: 15,
+    fontWeight: "regular",
+    color: "#AEA8B2",
+    maxWidth: 247,
+    marginBottom: 20,
+    textAlign: "center",
+  },
+
+  button: {
+    width: 280,
+    height: 54,
+    marginBottom: 90,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#896CFE",
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontWeight: "semibold",
+    fontSize: 18,
+  },
+});

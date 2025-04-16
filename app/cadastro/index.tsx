7821436315002ecf3bf35169b876514e99d57a03
MichelHoffmann <mic.hoff.jr@gmail.com>
2025-04-16 06:31:59 -0500

    style: finalizando estrutura da page cadastro

--- app/cadastro/index.tsx
+++ app/cadastro/index.tsx
@@ -1,11 +1,56 @@
-import { Text, View } from "react-native";
-import { styles } from "./style";
+import {
+  ImageBackground,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { style } from "./style";
+import { AtSign, Eye, Lock, UserRound } from "lucide-react-native";
 
 export default function Cadastro() {
-    return (
-        <View>
-            <Text style={styles.title}>RunFlow</Text>
+  return (
+    <View style={style.container}>
+      <ImageBackground
+        style={style.background}
+        source={require("../../assets/images/bg-form.png")}
+      >
+        <Text style={style.title}>RunFlow</Text>
+        <Text style={style.subTitle}>Crie a sua conta</Text>
+        <View style={style.inputContainer}>
+          <UserRound size={24} style={style.iconInput} />
+          <TextInput placeholder="Nome" style={style.input} />
         </View>
-    )
-}
 
+        <View style={style.inputContainer}>
+          <AtSign size={24} style={style.iconInput} />
+          <TextInput placeholder="Email" style={style.input} />
+        </View>
+
+        <View style={style.inputContainer}>
+          <Lock size={24} style={style.iconInput} />
+          <TextInput placeholder="Senha" style={style.input} />
+          <Eye size={24} style={style.iconPassword} />
+        </View>
+
+        <View style={style.inputContainer}>
+          <Lock size={24} style={style.iconInput} />
+          <TextInput placeholder="Confirme a senha" style={style.input} />
+          <Eye size={24} style={style.iconPassword} />
+        </View>
+        <Text></Text>
+        <TouchableOpacity>
+          <Text>Cadastrar</Text>
+        </TouchableOpacity>
+        <View>
+          <View></View>
+          <Text>Ou continue com</Text>
+          <View></View>
+        </View>
+        <TouchableOpacity>
+          <Text>Google</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </View>
+  );
+}
--- app/cadastro/style.ts
+++ app/cadastro/style.ts
@@ -1,10 +1,53 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({
+export const style = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#232323",
+  },
+  background: {
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+  },
   title: {
     fontSize: 30,
     fontWeight: "bold",
-    color: "#000",
-    marginTop: 20,
+    color: "#896CFE",
+  },
+  subTitle: {
+    fontWeight: "semibold",
+    fontSize: 20,
+    color: "#fff",
+  },
+  inputContainer: {
+    width: "80%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+  },
+  iconInput: {
+    position: "absolute",
+    left: 10,
+    color: "#676767",
+  },
+  iconPassword: {
+    position: "absolute",
+    right: 10,
+    color: "#676767",
+  },
+  input: {
+    width: "100%",
+    height: 48,
+    backgroundColor: "#282828",
+    color: "#676767",
+    borderRadius: 5,
+    fontWeight: "regular",
+    fontSize: 16,
+    paddingInline: 40,
   },
 });

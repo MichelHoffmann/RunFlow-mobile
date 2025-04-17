42907c5e99a89865cefea6f180ebd5222ec033b0
MichelHoffmann <mic.hoff.jr@gmail.com>
2025-04-17 07:53:54 -0500

    edbrb

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,41 +1,59 @@
-import { TextInput, TextInputProps, View } from "react-native"
-import styles from "./style"
-import { AtSign, Lock, UserRound } from "lucide-react-native"
-import { forwardRef, useState } from "react"
-import { Controller, UseControllerProps } from 'react-hook-form'
+import { TextInput, TextInputProps, View } from "react-native";
+import styles from "./style";
+import { AtSign, Eye, EyeOff, Lock, UserRound } from "lucide-react-native";
+import { forwardRef, useState } from "react";
+import { Controller, UseControllerProps } from "react-hook-form";
 
 type Props = {
-  icon: 'name' | 'email' | 'password',
-  formProps: UseControllerProps,
-  inputProps: TextInputProps
-}
+  icon: "name" | "email" | "password";
+  formProps: UseControllerProps;
+  inputProps: TextInputProps;
+};
 
-const Input = forwardRef<TextInput, Props> (({ icon, formProps, inputProps }, ref) => {
-  const [onFocus, setOnFocus] = useState(false)
-  return (
-    <Controller
-      render={({ field}) => (
-        <View style={styles.container}>
-          {icon === 'name' && <UserRound size={24} style={styles.iconInput} />}
-          {icon === 'email' && <AtSign size={24} style={styles.iconInput} />}
-          {icon === 'password' && <Lock size={24} style={styles.iconInput} />}
-          <TextInput
-            ref={ref}
-            value={field.value}
-            onChangeText={field.onChange}
-            placeholder={inputProps.placeholder}
-            placeholderTextColor={"#676767"}
-            style={onFocus ? styles.inputFocused && styles.input : styles.input}
-            secureTextEntry={icon === 'password' ? true : false}
-            onFocus={() => setOnFocus(true)}
-            onBlur={() => setOnFocus(false)}
-            {...inputProps}
-          />
-        </View>
-      )}
-      {...formProps}
-    />
-  )
-})
+const Input = forwardRef<TextInput, Props>(
+  ({ icon, formProps, inputProps }, ref) => {
+    const [onFocus, setOnFocus] = useState(false);
+    const [showPassword, setShowPassword] = useState(true);
+    const condition = icon === "password" && showPassword;
 
-export { Input }
+    function handleShowPassword() {
+      alert("Show password");
+    }
+    return (
+      <Controller
+        render={({ field }) => (
+          <View style={onFocus ? styles.containerFocused : styles.container}>
+            <View style={styles.inputInfo}>
+              {icon === "name" && <UserRound size={24} color={"#676767"} />}
+              {icon === "email" && <AtSign size={24} color={"#676767"} />}
+              {icon === "password" && <Lock size={24} color={"#676767"} />}
+              <TextInput
+                ref={ref}
+                value={field.value}
+                onChangeText={field.onChange}
+                placeholder={inputProps.placeholder}
+                placeholderTextColor={"#676767"}
+                cursorColor={"#896CFE"}
+                style={styles.input}
+                {...(icon === "password" ? { secureTextEntry: true } : {})}
+                onFocus={() => setOnFocus(true)}
+                onBlur={() => setOnFocus(false)}
+                {...inputProps}
+              />
+            </View>
+            {condition && (
+              <Eye
+                size={24}
+                color={"#676767"}
+                onPress={handleShowPassword}
+              />
+            )}
+          </View>
+        )}
+        {...formProps}
+      />
+    );
+  }
+);
+
+export { Input };
--- src/components/input/style.ts
+++ src/components/input/style.ts
@@ -1,34 +1,41 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        width: "100%",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        position: "relative",
-      },
-      iconInput: {
-        position: "absolute",
-        left: 10,
-        color: "#676767",
-      },
-      input: {
-        width: "100%",
-        height: 48,
-        backgroundColor: "#282828",
-        color: "#676767",
-        borderRadius: 5,
-        fontWeight: "regular",
-        fontSize: 16,
-        paddingLeft: 40,
-        paddingRight: 11,
-        outlineStyle: 'none',
-      },
-      inputFocused: {
-        borderWidth: 1,
-        borderColor: '#896CFE',
-      },
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#282828",
+    borderRadius: 5,
+    paddingInline: 11,
+    gap: 11,
+  },
+  containerFocused: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#282828",
+    borderRadius: 5,
+    paddingInline: 11,
+    borderWidth: 1,
+    borderColor: "#896CFE",
+  },
+  inputInfo: {
+    width: "85%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 11,
+  },
+  input: {
+    flex: 1,
+    height: 48,
+    color: "#676767",
+    fontWeight: "regular",
+    fontSize: 16,
+  },
 });
 
-export default styles
+export default styles;
